5679be5b9926a7031ac0ad52444f3742365bd1f3
Steffen Schweneker <steffen.schweneker@haw-hamburg.de>
2016-05-10 15:52:27 +0200

    aufgabe 3 1.2 begin

--- Certified_Tester_Praktikum_Termin_3/tests/Processing_Test.java
+++ Certified_Tester_Praktikum_Termin_3/tests/Processing_Test.java
@@ -47,7 +47,132 @@ public class Processing_Test {
     	pUnit.equal();
     	actual = pUnit.getNumberLine();
     	assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void basicMul() {
+    	double op1 = 2.0,
+    			op2 = 2.0,
+    			expected = 4.0,
+    			actual;
+    	actual = pUnit.mul(op1, op2);
+    	assertEquals(expected, actual, EPS);
+    }
+    
+    @Test
+    public void basicDiv() {
+    	double numerator = 2.0,
+    			denominator = 2.0,
+    			expected = 1.0,
+    			actual;
+    	actual = pUnit.div(numerator, denominator);
+    	assertEquals(expected, actual, EPS);
+    }
+    
+    @Test(expected = Exception.class)
+    public void divByZero() {
+    	double numerator = 2.0,
+    			denominator = 0.0;
+    	pUnit.div(numerator, denominator);
+    }
+    
+    @Test
+    public void basicPlus() {
+    	double op1 = 1.0,
+    			op2 = 1.0,
+    			expected = 2.0,
+    			actual;
+    	actual = pUnit.plus(op1, op2);
+    	assertEquals(expected, actual, EPS);
+    }
+    
+    @Test
+    public void basicPower() {
+    	double x = 2.0,
+    			a = 2.0,
+    			expected = 4.0,
+    			actual;
+    	actual = pUnit.power(x, a);
+    	assertEquals(expected, actual, EPS);
     	
+    	x = -1.0;
+    	expected = 1.0;
+    	actual = pUnit.power(x, a);
+    	assertEquals(expected, actual, EPS);
+    }
+    
+    @Test
+    public void basicSqrt() {
+    	double radicand = 9.0,
+    			expected = 3.0,
+    			actual;
+    	actual = pUnit.sqrt(radicand);
+    	assertEquals(expected, actual, EPS);
+    }
+    
+    @Test
+    public void getEquationLine() {
+    	String expected = "1.0",
+    			actual;
+    	store.setEquationLine(expected);
+    	actual = pUnit.getEquationLine();
+    	assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void appedDigit() {
+    	String in = "10";
+    	boolean actual;
+    	
+    	actual = pUnit.appendDigit(in);
+    	assertFalse(actual);
+    	
+    	String numberLineString = "1234567890123456789012";
+    	in = "1";
+    	
+    	store.setNumberLine(numberLineString);
+    	actual = pUnit.appendDigit(in);
+    	assertFalse(actual);
+    	
+    	numberLineString = "0";
+    	in = "0";
+    	
+    	store.setNumberLine(numberLineString);
+    	actual = pUnit.appendDigit(in);
+    	assertFalse(actual);
+    	
+    	numberLineString = "1";
+    	in = "1";
+    	
+    	store.setNumberLine(numberLineString);
+    	actual = pUnit.appendDigit(in);
+    	assertTrue(actual);
+    }
+    
+//    @Test
+//    public void appendDot() {
+//    	String numberLineString = "123456789012345678901";
+//    	boolean actual;
+//    	
+//    	store.setNumberLine(numberLineString);
+//    	actual = pUnit.appendDot();
+//    	assertFalse(actual);
+//    	
+//    	numberLineString = "0.";
+//    	
+//    	store.setNumberLine(numberLineString);
+//    	actual = pUnit.appendDot();
+//    	assertFalse(actual);
+//    }
+    
+    @Test
+    public void bla() {
+    	String numberLineString = "0";
+    	String expected = "0.";
+    	boolean actual;
+    	store.setNumberLine(numberLineString);
+    	actual = pUnit.appendDot();
+    	assertTrue(actual);
     }
     
     
